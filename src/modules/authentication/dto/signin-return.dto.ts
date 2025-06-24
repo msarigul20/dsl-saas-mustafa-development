f15b2b9aaa822c127cc1637c3e689904d4f54386
Mustafa SARIGÜL <m.sarigul199@gmail.com>
2025-06-25 01:17:52 +0300

    User usage removed

--- src/modules/authentication/dto/signin-return.dto.ts
+++ src/modules/authentication/dto/signin-return.dto.ts
@@ -1,8 +1,7 @@
-import { User } from "src/modules/user/user.schema";
 
 export class SignInReturnDto {
   success: boolean;
   message: string;
   token?: string; // Optional, if you want to return a JWT token or similar
-  user?: User;
+  user?: string;
 }
