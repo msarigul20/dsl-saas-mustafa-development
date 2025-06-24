9b8f899356c1bd4529d64fea4e10fbdcb43dccd1
Mustafa SARIGÜL <m.sarigul199@gmail.com>
2025-06-25 01:32:04 +0300

    UserType is used

--- src/modules/authentication/dto/signin-return.dto.ts
+++ src/modules/authentication/dto/signin-return.dto.ts
@@ -1,8 +1,8 @@
-import { User } from "src/modules/user/user.schema";
+import { UserType } from "src/modules/user/user.type";
 
 export class SignInReturnDto {
   success: boolean;
   message: string;
   token?: string; // Optional, if you want to return a JWT token or similar
-  user?: User;
+  user?: UserType;
 }
